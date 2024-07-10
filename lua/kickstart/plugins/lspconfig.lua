735de4a4dc07902186a8cc6581f872dafb90f7be
Mats <you@example.com>
2024-07-10 17:36:59 +0200

    basedpyright and ruff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -138,7 +138,8 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        basedpyright = {},
+        ruff = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
